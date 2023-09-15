c11b41e26a5a4960acfbba65f18e8af996a5ddc9
Salma-Alfinda <salma.alfinda@students.utdi.ac.id>
2023-09-15 09:28:02 +0000

    perubahan 1

---
+++ haii.py
@@ -0,0 +1,9 @@
+# Menyapa pengguna
+print("Selamat datang!")
+
+# Meminta pengguna untuk memasukkan nama
+nama = input("Siapa nama Anda? ")
+
+# Menampilkan pesan selamat datang dengan nama pengguna
+print(f"Selamat datang, {nama}!")
+
